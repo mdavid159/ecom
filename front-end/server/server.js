4a07078dec1f7780ae21bc46cb2af8a9bac33e20
mdavid159 <davidmuntean658@gmail.com>
2025-06-26 17:24:52 +0300

    Add files via upload

---
+++ front-end/server/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const stripe = require('stripe')('sk_test_51Qg9q7FRSCuMVdLxymXde1DQeyU9kbClhHnyqW4rTytTuutAmqhqfE8yTEr0RZO3kVb0R6rj23HOnb7OL0hgOtVH00Br9SXHxR');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cors({ origin: 'http://localhost:4200', credentials: true }));
+
+app.post('/checkout', async (req, res, next) => {
+  try {
+    if (!req.body.items || !Array.isArray(req.body.items)) {
+      return res.status(400).json({ error: 'Invalid items in request body.' });
+    }
+
+    const lineItems = req.body.items.map((item) => {
+      if (!item.name || !item.price || typeof item.price !== 'number') {
+        throw new Error('Invalid item format.');
+      }
+
+      console.log('Image url:', `http://localhost:3000${item.imageUrl}`);
+      console.log('Url:', item.imageUrl);
+
+      return {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: item.name,
+            images: [],
+          },
+          unit_amount: item.price * 100,
+        },
+        quantity: item.quantity
+      };
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: 'http://localhost:4200/checkout',
+      cancel_url: 'http://localhost:4200/cancel',
+    });
+
+    res.status(200).json({ id: session.id });
+  } catch (error) {
+    console.error('Error in /checkout:', error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(4242, () => console.log('Server running on port 4242'));
